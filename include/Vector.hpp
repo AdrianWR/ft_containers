ef40ca8a7b18c9c4b35e468722733da2424d9960
Adrian Roque <adrian.w.roque@gmail.com>
2022-02-09 22:19:20 -0300

    Initial commit

---
+++ include/Vector.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+template <typename T>
+class Vector {
+    Vector();
+    Vector(const Vector&);
+    Vector& operator=(const Vector&);
+    ~Vector();
+};
+
+#endif
+
