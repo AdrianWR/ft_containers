fcd70bce3602791acaf3109df4d41cf2582b2137
Adrian Roque <adrian.w.roque@gmail.com>
2022-02-10 22:19:56 -0300

    Add Vector container

--- include/Vector.hpp
+++ include/Vector.hpp
@@ -3,13 +3,44 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
-template <typename T>
-class Vector {
-    Vector();
-    Vector(const Vector&);
-    Vector& operator=(const Vector&);
-    ~Vector();
+namespace ft {
+
+template <class T, class Alloc = std::allocator<T>> class Vector {
+  public:
+    typedef T value_type;
+    typedef Alloc allocator_type;
+    typedef typename allocator_type::reference reference;
+    typedef typename allocator_type::const_reference const_reference;
+    typedef typename allocator_type::pointer pointer;
+    typedef typename allocator_type::const_pointer const_pointer;
+    typedef std::ptrdiff_t difference_type;
+    typedef std::size_t size_type;
+
+  private:
+    T *data;
+    size_type _size;
+    size_type _capacity;
+
+  public:
+    Vector() : _size(0){};
+
+    Vector(unsigned int size, T value = 0) : _size(size) {
+	data = Alloc::allocate(size);
+	for (unsigned int i = 0; i < size; i++) {
+	    data[i] = value;
+	}
+    }
+
+    Vector(const Vector &);
+
+    Vector &operator=(const Vector &);
+
+    ~Vector(){};
+
+    unsigned int size() const { return this->_size; };
 };
+
+} // namespace ft
 
 #endif
 
