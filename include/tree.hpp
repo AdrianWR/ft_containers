01242fb39c005571439106e5f7b1afbd686d0650
Adrian Roque <adrian.w.roque@gmail.com>
2022-03-24 00:13:45 -0300

    Add node static iterators

--- include/tree.hpp
+++ include/tree.hpp
@@ -8,10 +8,13 @@
 #include <cstddef>
 #include <memory>
 
+#include <iostream>
+
 namespace ft {
 
 enum color { RED, BLACK };
-template <class T> class Node {
+
+template <class T, class Alloc = std::allocator<T>> class Node {
 public:
   typedef T value_type;
   typedef T *pointer;
@@ -22,116 +25,204 @@ public:
   typedef Node<T> &node_ref;
   typedef const Node<T> *const_node_ptr;
   typedef const Node<T> &const_node_ref;
+  typedef Alloc allocator_type;
 
 private:
-  value_type _value;
+  // Properties
   node_ptr _parent;
   node_ptr _left;
   node_ptr _right;
-  color _color;
+  enum color _color;
+  pointer _value_ptr;
+  allocator_type _alloc;
 
 public:
-  node_ptr next() {
-    pointer x = this;
-    if (x->right != nullptr) {
-      x = x->right;
-      while (x->left != nullptr)
-        x = x->left;
-    } else {
-      pointer y = x->parent;
-      while (x == y->right) {
-        x = y;
-        y = y->parent;
+  // Default Constructor
+  explicit Node(node_ptr parent = _nullptr, node_ptr left = _nullptr,
+                node_ptr right = _nullptr, enum color color = RED,
+                allocator_type alloc = allocator_type())
+      : _value_ptr(_nullptr), _parent(parent), _left(left), _right(right),
+        _color(color), _alloc(alloc) {}
+
+  // Value constructor
+  Node(value_type value, enum color color = RED, node_ptr parent = _nullptr,
+       node_ptr left = _nullptr, node_ptr right = _nullptr,
+       allocator_type alloc = allocator_type())
+      : _parent(parent), _left(left), _right(right), _color(color) {
+    _value_ptr = _alloc.allocate(1);
+    _alloc.construct(_value_ptr, value);
+  }
+
+  // Copy constructor
+  Node(const Node &other)
+      : _value_ptr(_nullptr), _parent(other._parent), _left(other._left),
+        _right(other._right), _color(other._color), _alloc(other._alloc) {
+    if (other._value_ptr != _nullptr) {
+      _value_ptr = _alloc.allocate(1);
+      _alloc.construct(_value_ptr, *other._value_ptr);
+    }
+  }
+
+  // Copy assignment operator
+  Node &operator=(const Node &other) {
+    if (this != &other) {
+      if (other._value_ptr != _nullptr) {
+        if (_value_ptr != _nullptr) {
+          _alloc.destroy(_value_ptr);
+          _alloc.deallocate(_value_ptr, 1);
+        }
+        _value_ptr = _alloc.allocate(1);
+        _alloc.construct(_value_ptr, *other._value_ptr);
+      } else {
+        if (_value_ptr != _nullptr) {
+          _alloc.destroy(_value_ptr);
+          _alloc.deallocate(_value_ptr, 1);
+        }
+        _value_ptr = _nullptr;
       }
-      if (x->right != y)
+      _parent = other._parent;
+      _left = other._left;
+      _right = other._right;
+      _color = other._color;
+    }
+    return *this;
+  }
+
+  // Default destructor
+  virtual ~Node() {
+    if (_value_ptr != _nullptr) {
+      _alloc.destroy(_value_ptr);
+      _alloc.deallocate(_value_ptr, 1);
+    }
+  }
+
+  // Getters
+
+  reference value() { return *_value_ptr; }
+
+  const_reference value() const { return *_value_ptr; }
+
+  node_ptr parent() { return _parent; }
+
+  const_node_ptr parent() const { return _parent; }
+
+  node_ptr left() { return _left; }
+
+  const_node_ptr left() const { return _left; }
+
+  node_ptr right() { return _right; }
+
+  const_node_ptr right() const { return _right; }
+
+  enum color color() { return _color; }
+
+  const_node_ptr color() const { return _color; }
+
+  // Setters
+
+  void value(value_type value) {
+    if (_value_ptr == _nullptr) {
+      _value_ptr = _alloc.allocate(1);
+    } else {
+      _alloc.destroy(_value_ptr);
+    }
+    _alloc.construct(_value_ptr, value);
+  }
+
+  void left(node_ptr left) { _left = left; }
+
+  void right(node_ptr right) { _right = right; }
+
+  void parent(node_ptr parent) { _parent = parent; }
+
+  node_ptr next() {
+    node_ptr x;
+    node_ptr y;
+    x = this;
+
+    if (x->right() != _nullptr) {
+      x = x->right();
+      while (x->left() != _nullptr)
+        x = x->left();
+    } else {
+      y = x->parent();
+      while (x == y->right()) {
+        x = y;
+        y = y->parent();
+      }
+      if (x->right() != y)
         x = y;
     }
     return x;
   }
 
   node_ptr previous() {
-    pointer x = this;
-    if (x->color == ft::color::RED && x->parent->parent == x) {
-      x = x->right;
-    } else if (x->left != nullptr) {
-      pointer y = x->left;
-      while (y->right != nullptr)
-        y = y->right;
+    node_ptr x;
+    node_ptr y;
+    x = this;
+
+    if (x->color() == ft::color::RED && x->parent()->parent() == x) {
+      x = x->right();
+    } else if (x->left() != _nullptr) {
+      y = x->left();
+      while (y->right() != _nullptr)
+        y = y->right();
       x = y;
     } else {
-      pointer y = x->parent;
-      while (x == y->left) {
+      y = x->parent();
+      while (x == y->left()) {
         x = y;
-        y = y->parent;
+        y = y->parent();
       }
       x = y;
     }
     return x;
   }
 
-  // Default constructor
-  Node()
-      : _parent(_nullptr), _left(_nullptr), _right(_nullptr), _color(BLACK) {}
-
-  // Constructor
-  Node(node_ptr parent = _nullptr, node_ptr left = _nullptr,
-       node_ptr right = _nullptr, color color = RED)
-      : _parent(parent), _left(left), _right(right), _color(color) {}
-
-  // Value constructor
-  Node(value_type value, node_ptr parent = _nullptr, node_ptr left = _nullptr,
-       node_ptr right = _nullptr, color color = RED)
-      : _value(value), _parent(parent), _left(left), _right(right),
-        _color(color) {}
-
-  // Copy constructor
-  Node(const Node &node) {
-    _value = node._value;
-    _parent = node._parent;
-    _left = node._left;
-    _right = node._right;
-    _color = node._color;
+  node_ptr minimum() {
+    node_ptr x = this;
+    while (x->left() != _nullptr) {
+      x = x->left();
+    }
+    return x;
   }
 
-  // Copy assignment operator
-  Node &operator=(const Node &node) {
-    if (this != &node) {
-      _value = node._value;
-      _parent = node._parent;
-      _left = node._left;
-      _right = node._right;
-    }
-    return *this;
-  };
-
-  // Default destructor
-  virtual ~Node() {}
+  node_ptr maximum() {
+    node_ptr x = this;
+    while (x->right() != _nullptr)
+      x = x->right();
+    return x;
+  }
 
   // Comparison operators
-  bool operator==(const Node &node) const { return _value == node._value; }
+  // bool operator==(const Node &node) const { return _value == node.value; }
 
-  bool operator!=(const Node &node) const { return _value != node._value; }
+  // bool operator!=(const Node &node) const { return value != node.value; }
 
-  bool operator<(const Node &node) const { return _value < node._value; }
+  // bool operator<(const Node &node) const { return value < node.value; }
 
-  bool operator>(const Node &node) const { return _value > node._value; }
+  // bool operator>(const Node &node) const { return value > node.value; }
 
-  bool operator<=(const Node &node) const { return _value <= node._value; }
+  // bool operator<=(const Node &node) const { return value <= node.value; }
 
-  bool operator>=(const Node &node) const { return _value >= node._value; }
-
-  // Getters
-
-  value_type get_value() const { return _value; }
-
-  node_ptr get_parent() const { return _parent; }
-
-  node_ptr get_left() const { return _left; }
-
-  node_ptr get_right() const { return _right; }
-
-  color get_color() const { return _color; }
+  // bool operator>=(const Node &node) const { return value >= node.value; }
 };
+
+template <class T> bool operator==(const Node<T> &lhs, const Node<T> &rhs) {
+  return lhs.value == rhs.value;
+}
+
+template <class T> bool operator!=(const Node<T> &lhs, const Node<T> &rhs) {
+  return !(lhs == rhs);
+}
+
+template <class T> bool operator<(const Node<T> &lhs, const Node<T> &rhs) {
+  return lhs.get_value() < rhs.get_value();
+}
+
+template <class T> bool operator>(const Node<T> &lhs, const Node<T> &rhs) {
+  return lhs.get_value() > rhs.get_value();
+}
 
 template <class T>
 class RBTreeIterator : public ft::iterator<ft::bidirectional_iterator_tag, T> {
@@ -153,7 +244,7 @@ public:
 
   RBTreeIterator(node_type node) : _node(node) {}
 
-  reference operator*() const { return _node->_value; }
+  reference operator*() const { return _node->value; }
 
   pointer operator->() const { return &(operator*()); }
 
@@ -184,7 +275,7 @@ public:
   bool operator!=(const self &it) const { return _node != it._node; }
 };
 
-template <class T, class Compare = ft::less<T>, class Node = ft::Node<T>,
+template <class T, class Compare = ft::less<T>, class Node = Node<T>,
           class Alloc = std::allocator<T>,
           class NodeAlloc = std::allocator<Node>>
 class RedBlackTree {
@@ -204,41 +295,67 @@ public:
   typedef Compare key_compare;
   typedef size_t size_type;
   typedef ptrdiff_t difference_type;
+  typedef RBTreeIterator<T> iterator;
+  typedef RBTreeIterator<const T> const_iterator;
+  typedef ft::reverse_iterator<iterator> reverse_iterator;
+  typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
 
 private:
   node_ptr _root;
+  allocator_type _alloc;
   node_allocator_type _node_alloc;
-  static node_ptr _nil = Node();
 
 public:
   // Default constructor
-  RedBlackTree(const node_allocator_type &node_alloc = node_allocator_type())
+  explicit RedBlackTree(
+      const node_allocator_type &node_alloc = node_allocator_type(),
+      const allocator_type &alloc = allocator_type())
+      : _node_alloc(node_alloc), _alloc(alloc) {
+    _root = _node_alloc.allocate(1);
+    _node_alloc.construct(_root, _nullptr, _nullptr, _nullptr, ft::color::BLACK,
+                          _alloc);
+  }
+
+  // Value constructor
+  explicit RedBlackTree(
+      const value_type &value,
+      const node_allocator_type &node_alloc = node_allocator_type(),
+      const allocator_type &alloc = allocator_type())
       : _node_alloc(node_alloc) {
     _root = _node_alloc.allocate(1);
-    _node_alloc.construct(_root, _nullptr, _nullptr, _nullptr);
+    _node_alloc.construct(_root, node_type(value, _nullptr, _nullptr, _nullptr,
+                                           ft::color::BLACK, _alloc));
   }
 
   // Copy constructor
-  RedBlackTree(const RedBlackTree &tree) : _node_alloc(tree._node_alloc) {
-    _root = _node_alloc.allocate(1);
-    _node_alloc.construct(_root, _nullptr, _nullptr, _nullptr);
-    _root = tree._root;
-  }
+  // RedBlackTree(const RedBlackTree &tree)
+  //    : _node_alloc(tree._node_alloc), _alloc(tree._alloc) {
+  //  _root = _node_alloc.allocate(1);
+  //  _node_alloc.construct(_root, _nullptr, _nullptr, _nullptr,
+  //  ft::color::BLACK,
+  //                        _alloc);
+  //  for (const_iterator it = tree.begin(); it != tree.end(); ++it) {
+  //    insert(*it);
+  //  }
+  //}
 
   // Destructor
-  virtual ~RedBlackTree() {
-    _node_alloc.destroy(_root);
-    _node_alloc.deallocate(_root, 1);
-  }
+  virtual ~RedBlackTree() {}
 
-  // Copy assignment operator
-  RedBlackTree &operator=(const RedBlackTree &tree) {
-    if (this != &tree) {
-      _node_alloc = tree._node_alloc;
-      _root = tree._root;
-    }
-    return *this;
-  }
+  //// Copy assignment operator
+  // RedBlackTree &operator=(const RedBlackTree &tree) {
+  //   if (this != &tree) {
+  //     _node_alloc = tree._node_alloc;
+  //     _root = tree._root;
+  //   }
+  //   return *this;
+  // }
+
+  // Getters
+
+  node_ptr get_root() const { return _root; }
+
+  node_ptr get_nil() const { return _nullptr; }
 
   void left_rotate(node_ptr x) {
     node_ptr y = x->right;
@@ -279,7 +396,7 @@ public:
     node_ptr x = _root;
     while (x != _nullptr) {
       y = x;
-      if (z->_value < x->_value)
+      if (z->value < x->value)
         x = x->left;
       else
         x = x->right;
@@ -287,7 +404,7 @@ public:
     z->parent = y;
     if (y == _nullptr)
       _root = z;
-    else if (z->_value < y->_value)
+    else if (z->value < y->value)
       y->left = z;
     else
       y->right = z;
--- test/TestTree.cpp
+++ test/TestTree.cpp
@@ -1,13 +1,183 @@
 #include <gtest/gtest.h>
+#include <memory>
 
 #include "nullptr.hpp"
 #include "tree.hpp"
 
-TEST(TestTree, TestTree) { EXPECT_EQ(1, 1); }
+#include <iostream>
 
-TEST(TestNode, TestNode) {
+class TestNodeIterator : public ::testing::Test {
+
+public:
+  ft::Node<int> *n13;
+  ft::Node<int> *n8;
+  ft::Node<int> *n1;
+  ft::Node<int> *n6;
+  ft::Node<int> *n11;
+  ft::Node<int> *n17;
+  ft::Node<int> *n15;
+  ft::Node<int> *n25;
+  ft::Node<int> *n22;
+  ft::Node<int> *n27;
+
+  TestNodeIterator() {}
+
+  void SetUp() {
+    n13 = new ft::Node<int>(13, ft::color::BLACK);
+    n8 = new ft::Node<int>(8, ft::color::RED);
+    n1 = new ft::Node<int>(1, ft::color::BLACK);
+    n6 = new ft::Node<int>(6, ft::color::RED);
+    n11 = new ft::Node<int>(11, ft::color::BLACK);
+    n17 = new ft::Node<int>(17, ft::color::RED);
+    n15 = new ft::Node<int>(15, ft::color::BLACK);
+    n25 = new ft::Node<int>(25, ft::color::BLACK);
+    n22 = new ft::Node<int>(22, ft::color::RED);
+    n27 = new ft::Node<int>(27, ft::color::RED);
+    n13->parent(ft::_nullptr);
+    n13->left(n8);
+    n13->right(n17);
+    n8->parent(n13);
+    n8->left(n1);
+    n8->right(n11);
+    n1->parent(n8);
+    n1->left(ft::_nullptr);
+    n1->right(n6);
+    n6->parent(n1);
+    n6->left(ft::_nullptr);
+    n6->right(ft::_nullptr);
+    n11->parent(n8);
+    n11->left(ft::_nullptr);
+    n11->right(ft::_nullptr);
+    n17->parent(n13);
+    n17->left(n15);
+    n17->right(n25);
+    n15->parent(n17);
+    n15->left(ft::_nullptr);
+    n15->right(ft::_nullptr);
+    n25->parent(n17);
+    n25->left(n22);
+    n25->right(n27);
+    n22->parent(n25);
+    n22->left(ft::_nullptr);
+    n22->right(ft::_nullptr);
+    n27->parent(n25);
+    n27->left(ft::_nullptr);
+    n27->right(ft::_nullptr);
+  }
+
+  void TearDown() {
+    delete n6;
+    delete n1;
+    delete n8;
+    delete n11;
+    delete n22;
+    delete n27;
+    delete n25;
+    delete n15;
+    delete n17;
+    delete n13;
+  }
+};
+
+TEST(TestNode, TestNodeConstructor) {
+  ft::Node<int> node_zero;
+  EXPECT_EQ(node_zero.parent(), ft::_nullptr);
+  EXPECT_EQ(node_zero.left(), ft::_nullptr);
+  EXPECT_EQ(node_zero.right(), ft::_nullptr);
+
   ft::Node<int> node(1);
-  EXPECT_EQ(node.get_value(), 1);
-  EXPECT_EQ(node.get_left(), ft::_nullptr);
-  EXPECT_EQ(node.get_right(), ft::_nullptr);
+  EXPECT_EQ(node.value(), 1);
+  EXPECT_EQ(node.left(), ft::_nullptr);
+  EXPECT_EQ(node.right(), ft::_nullptr);
+  EXPECT_EQ(node.parent(), ft::_nullptr);
+
+  ft::Node<int> node2(2, ft::color::RED, &node);
+  EXPECT_EQ(node2.value(), 2);
+  EXPECT_EQ(node2.parent(), &node);
+  EXPECT_EQ(node2.left(), ft::_nullptr);
+  EXPECT_EQ(node2.right(), ft::_nullptr);
+  EXPECT_EQ(node2.color(), ft::color::RED);
+
+  ft::Node<int> node3(node2);
+  EXPECT_EQ(node3.value(), 2);
+  EXPECT_EQ(node3.parent(), &node);
+  EXPECT_EQ(node3.left(), ft::_nullptr);
+  EXPECT_EQ(node3.right(), ft::_nullptr);
+  EXPECT_EQ(node3.color(), ft::color::RED);
+
+  ft::Node<int> node4(node);
+  EXPECT_EQ(node4.value(), 1);
+  EXPECT_EQ(node4.parent(), ft::_nullptr);
+  EXPECT_EQ(node4.left(), ft::_nullptr);
+  EXPECT_EQ(node4.right(), ft::_nullptr);
+  EXPECT_EQ(node4.color(), ft::color::RED);
+
+  ft::Node<int> node5;
+  node5 = node;
+  EXPECT_EQ(node5.value(), 1);
+  EXPECT_EQ(node5.parent(), ft::_nullptr);
+  EXPECT_EQ(node5.left(), ft::_nullptr);
+  EXPECT_EQ(node5.right(), ft::_nullptr);
+  EXPECT_EQ(node5.color(), ft::color::RED);
+
+  node5 = node2;
+  EXPECT_EQ(node5.value(), 2);
+  EXPECT_EQ(node5.parent(), &node);
+  EXPECT_EQ(node5.left(), ft::_nullptr);
+  EXPECT_EQ(node5.right(), ft::_nullptr);
+  EXPECT_EQ(node5.color(), ft::color::RED);
 }
+
+TEST_F(TestNodeIterator, TestNodeIterator) {
+  ft::Node<int> *root = n13;
+  EXPECT_EQ(root->minimum(), n1);
+  EXPECT_EQ(root->maximum(), n27);
+  EXPECT_EQ(root->previous(), n11);
+  EXPECT_EQ(root->next(), n15);
+
+  EXPECT_EQ(n1->next(), n6);
+  EXPECT_EQ(n6->next(), n8);
+  EXPECT_EQ(n8->next(), n11);
+  EXPECT_EQ(n11->next(), n13);
+  EXPECT_EQ(n13->next(), n15);
+  EXPECT_EQ(n15->next(), n17);
+  EXPECT_EQ(n17->next(), n22);
+  EXPECT_EQ(n22->next(), n25);
+  EXPECT_EQ(n25->next(), n27);
+
+  EXPECT_EQ(n27->previous(), n25);
+  EXPECT_EQ(n25->previous(), n22);
+  EXPECT_EQ(n22->previous(), n17);
+  EXPECT_EQ(n17->previous(), n15);
+  EXPECT_EQ(n15->previous(), n13);
+  EXPECT_EQ(n13->previous(), n11);
+  EXPECT_EQ(n11->previous(), n8);
+  EXPECT_EQ(n8->previous(), n6);
+  EXPECT_EQ(n6->previous(), n1);
+}
+
+// TEST(TestTree, TestTreeDefaultConstructor) {
+//   ft::RedBlackTree<int> tree;
+//   EXPECT_EQ(tree.get_root()->parent, ft::_nullptr);
+//   EXPECT_EQ(tree.get_root()->left, ft::_nullptr);
+//   EXPECT_EQ(tree.get_root()->right, ft::_nullptr);
+// }
+//
+// TEST(TestTree, TestTreeValueConstructor) {
+//   ft::RedBlackTree<int> tree(42);
+//   EXPECT_EQ(tree.get_root()->value(), 42);
+//   EXPECT_EQ(tree.get_root()->parent, ft::_nullptr);
+//   EXPECT_EQ(tree.get_root()->left, ft::_nullptr);
+//   EXPECT_EQ(tree.get_root()->right, ft::_nullptr);
+//   EXPECT_EQ(tree.get_root()->color, ft::color::BLACK);
+// }
+
+// TEST(TestTree, TestTreeCopyConstructor) {
+//   ft::RedBlackTree<int> tree(42);
+//   ft::RedBlackTree<int> tree_copy(tree);
+//   EXPECT_EQ(tree_copy.get_root()->value(), 42);
+//   EXPECT_EQ(tree_copy.get_root()->parent, ft::_nullptr);
+//   EXPECT_EQ(tree_copy.get_root()->left, ft::_nullptr);
+//   EXPECT_EQ(tree_copy.get_root()->right, ft::_nullptr);
+//   EXPECT_EQ(tree_copy.get_root()->color, ft::color::BLACK);
+// }
