a44b19ede1bc0a820372b4eb421e3dab6d26b7aa
Adrian Roque <adrian.w.roque@gmail.com>
2022-04-03 23:52:49 -0300

    Add stack container adaptor

---
+++ include/stack.hpp
@@ -0,0 +1,102 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include <vector.hpp>
+
+namespace ft {
+
+template <class T, class Container = vector<T>> class stack {
+
+public:
+  typedef T value_type;
+  typedef Container container_type;
+  typedef typename Container::size_type size_type;
+
+  template <typename T1, typename Container1>
+  friend bool operator==(const stack<T1, Container1> &,
+                         const stack<T1, Container1> &);
+
+  template <typename T1, typename Container1>
+  friend bool operator<(const stack<T1, Container1> &,
+                        const stack<T1, Container1> &);
+
+protected:
+  Container _container;
+
+public:
+  /**
+   * @brief Construct a new stack object
+   *
+   * @param container The container to use as the underlying data structure
+   */
+  explicit stack(const container_type &ctnr = container_type())
+      : _container(ctnr) {}
+
+  /**
+   * @brief Check if the stack is empty
+   */
+  bool empty() const { return _container.empty(); }
+
+  /**
+   * @brief Get the size of the stack
+   */
+  size_type size() const { return _container.size(); }
+
+  /**
+   * @brief Get the top element of the stack
+   */
+
+  value_type &top() { return _container.back(); }
+
+  const value_type &top() const { return _container.back(); }
+
+  /**
+   * @brief Push an element onto the stack
+   *
+   * @param value The value to push onto the stack
+   */
+  void push(const value_type &value) { _container.push_back(value); }
+
+  /**
+   * @brief Pop an element off the stack
+   */
+  void pop() { _container.pop_back(); }
+};
+
+template <class T, class Container>
+bool operator==(const stack<T, Container> &lhs,
+                const stack<T, Container> &rhs) {
+  return lhs._container == rhs._container;
+}
+
+template <class T, class Container>
+bool operator!=(const stack<T, Container> &lhs,
+                const stack<T, Container> &rhs) {
+  return !(lhs == rhs);
+}
+
+template <class T, class Container>
+bool operator<(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+  return lhs._container < rhs._container;
+}
+
+template <class T, class Container>
+bool operator>(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+  return rhs < lhs;
+}
+
+template <class T, class Container>
+bool operator<=(const stack<T, Container> &lhs,
+                const stack<T, Container> &rhs) {
+  return !(rhs < lhs);
+}
+
+template <class T, class Container>
+bool operator>=(const stack<T, Container> &lhs,
+                const stack<T, Container> &rhs) {
+  return !(lhs < rhs);
+}
+
+} // namespace ft
+
+#endif
---
+++ test/TestStack.cpp
@@ -0,0 +1,169 @@
+#include "stack.hpp"
+#include "vector.hpp"
+#include <gtest/gtest.h>
+#include <vector>
+
+TEST(TestStack, TestConstructor) {
+  ft::vector<int> v;
+  ft::vector<int> v1(10, 42);
+  std::vector<int> v2(10, 42);
+
+  ft::stack<int> s(v);
+  ft::stack<int> s1(v1);
+  ft::stack<int, std::vector<int>> s2(v2);
+  ASSERT_EQ(s.size(), 0);
+  ASSERT_EQ(s1.size(), 10);
+  ASSERT_EQ(s2.size(), 10);
+}
+
+TEST(TestStack, TestPush) {
+  ft::vector<int> v;
+  ft::vector<int> v1(10, 42);
+  std::vector<int> v2(10, 42);
+
+  ft::stack<int> s(v);
+  ft::stack<int> s1(v1);
+  ft::stack<int, std::vector<int>> s2(v2);
+  ASSERT_EQ(s.size(), 0);
+  ASSERT_EQ(s1.size(), 10);
+  ASSERT_EQ(s2.size(), 10);
+  s.push(42);
+  s1.push(42);
+  s2.push(42);
+  ASSERT_EQ(s.size(), 1);
+  ASSERT_EQ(s1.size(), 11);
+  ASSERT_EQ(s2.size(), 11);
+}
+
+TEST(TestStack, TestTop) {
+  ft::vector<int> v;
+  ft::vector<int> v1(10, 42);
+  std::vector<int> v2(10, 42);
+
+  ft::stack<int> s(v);
+  ft::stack<int> s1(v1);
+  ft::stack<int, std::vector<int>> s2(v2);
+  ASSERT_EQ(s.size(), 0);
+  ASSERT_EQ(s1.size(), 10);
+  ASSERT_EQ(s2.size(), 10);
+  s.push(43);
+  s1.push(43);
+  s2.push(43);
+  ASSERT_EQ(s.top(), 43);
+  ASSERT_EQ(s1.top(), 43);
+  ASSERT_EQ(s2.top(), 43);
+}
+
+TEST(TestStack, TestPop) {
+  ft::vector<int> v;
+  ft::vector<int> v1(10, 42);
+  std::vector<int> v2(10, 42);
+
+  ft::stack<int> s(v);
+  ft::stack<int> s1(v1);
+  ft::stack<int, std::vector<int>> s2(v2);
+  ASSERT_EQ(s.size(), 0);
+  ASSERT_EQ(s1.size(), 10);
+  ASSERT_EQ(s2.size(), 10);
+  s.push(43);
+  s1.push(43);
+  s2.push(43);
+  ASSERT_EQ(s.size(), 1);
+  ASSERT_EQ(s1.size(), 11);
+  ASSERT_EQ(s2.size(), 11);
+  s.pop();
+  s1.pop();
+  s2.pop();
+  ASSERT_EQ(s.size(), 0);
+  ASSERT_EQ(s1.size(), 10);
+  ASSERT_EQ(s2.size(), 10);
+}
+
+TEST(TestStack, TestEmpty) {
+  ft::vector<int> v;
+  ft::vector<int> v1(10, 42);
+  std::vector<int> v2(10, 42);
+
+  ft::stack<int> s(v);
+  ft::stack<int> s1(v1);
+  ft::stack<int, std::vector<int>> s2(v2);
+  ASSERT_EQ(s.size(), 0);
+  ASSERT_EQ(s1.size(), 10);
+  ASSERT_EQ(s2.size(), 10);
+  ASSERT_TRUE(s.empty());
+  ASSERT_FALSE(s1.empty());
+  ASSERT_FALSE(s2.empty());
+}
+
+TEST(TestStack, TestSize) {
+  ft::vector<int> v;
+  ft::vector<int> v1(10, 42);
+  std::vector<int> v2(10, 42);
+
+  ft::stack<int> s(v);
+  ft::stack<int> s1(v1);
+  ft::stack<int, std::vector<int>> s2(v2);
+  ASSERT_EQ(s.size(), 0);
+  ASSERT_EQ(s1.size(), 10);
+  ASSERT_EQ(s2.size(), 10);
+  ASSERT_EQ(s.size(), 0);
+  ASSERT_EQ(s1.size(), 10);
+  ASSERT_EQ(s2.size(), 10);
+  s.push(43);
+  s1.push(43);
+  s2.push(43);
+  ASSERT_EQ(s.size(), 1);
+  ASSERT_EQ(s1.size(), 11);
+  ASSERT_EQ(s2.size(), 11);
+  s.pop();
+  s1.pop();
+  s2.pop();
+  ASSERT_EQ(s.size(), 0);
+  ASSERT_EQ(s1.size(), 10);
+  ASSERT_EQ(s2.size(), 10);
+}
+
+#include <stack>
+
+TEST(TestStack, TestComparisonOperators) {
+  ft::vector<int> v;
+  ft::vector<int> v1(10, 42);
+  ft::vector<int> v2(10, 42);
+
+  ft::stack<int> s(v);
+  ft::stack<int> s1(v1);
+  ft::stack<int, ft::vector<int>> s2(v2);
+  ASSERT_EQ(s.size(), 0);
+  ASSERT_EQ(s1.size(), 10);
+  ASSERT_EQ(s2.size(), 10);
+  ASSERT_TRUE(s == s);
+  ASSERT_FALSE(s != s);
+  ASSERT_FALSE(s == s1);
+  ASSERT_TRUE(s != s1);
+  ASSERT_FALSE(s == s2);
+  ASSERT_TRUE(s != s2);
+  s.push(43);
+  s1.push(43);
+  s2.push(43);
+  ASSERT_EQ(s.size(), 1);
+  ASSERT_EQ(s1.size(), 11);
+  ASSERT_EQ(s2.size(), 11);
+  ASSERT_TRUE(s == s);
+  ASSERT_FALSE(s != s);
+  ASSERT_FALSE(s == s1);
+  ASSERT_TRUE(s != s1);
+  ASSERT_FALSE(s == s2);
+  ASSERT_TRUE(s != s2);
+  s.pop();
+  s1.pop();
+  s2.pop();
+  ASSERT_EQ(s.size(), 0);
+  ASSERT_EQ(s1.size(), 10);
+  ASSERT_EQ(s2.size(), 10);
+  ASSERT_TRUE(s == s);
+  ASSERT_FALSE(s != s);
+  ASSERT_FALSE(s == s1);
+  ASSERT_TRUE(s != s1);
+  ASSERT_FALSE(s == s2);
+  ASSERT_TRUE(s != s2);
+}
